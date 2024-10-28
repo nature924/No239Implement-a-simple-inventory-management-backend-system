ec442dafe0fbb2c883ee12fadadc150dff41c4a0
nature924 <1173016385@qq.com>
2024-10-28 22:29:24 +0800

    基于Spring Boot实现简单的进销存后台系统

---
+++ src/main/java/com/dev/warehouse/sys/config/ResourcesConfig.java
@@ -0,0 +1,21 @@
+package com.dev.warehouse.sys.config;
+
+import com.dev.warehouse.sys.common.Constants;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ResourcesConfig implements WebMvcConfigurer {
+
+    @Value("${yuanlrc.photoFilePath}")
+    private String filePath;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry)
+    {
+        /** 本地文件上传路径 */
+        registry.addResourceHandler(Constants.RESOURCE_PREFIX + "/**").addResourceLocations("file:" + filePath );
+    }
+}
